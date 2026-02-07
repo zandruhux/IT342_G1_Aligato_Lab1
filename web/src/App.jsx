813de4a3fd3a0714d1fbe822ce22881495946c68
LAB13_USER ON WS39 <L13Y09W39@LabHCI.net>
2026-02-07 08:38:58 +0800

    chore(frontend): initialized frontend folder structure

---
+++ web/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-900">
+      <h1 className="text-5xl font-bold text-cyan-400">
+        Sample Webpage 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
